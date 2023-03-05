489e03972f78eca4e57a16553dec1959f484b0b8
swmac <shaynefw@gmail.com>
2023-03-05 15:25:50 -0500

    new attempt to rebuild structure and functionality

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiController = require('./api');
-const homeController = require('./homeController.js');
-
-router.use('/', homeController);
-router.use('/api', apiController);
-
-module.exports = router;
