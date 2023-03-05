b301a7d6461fe63e4e76a873c25df87f67893a08
swmac <shaynefw@gmail.com>
2023-03-05 18:08:51 -0500

    added server js

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const session = require('express-session');
+const Handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({
+  helpers: {
+    formatDate: function (date) {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+  },
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Import routes and give the server access to them
+const routes = require('./controllers');
+app.use(routes);
+
+// Sync sequelize models to the database, then start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+});
+
