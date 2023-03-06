c36d3a0f28a558fdc24dd508e6e5c2040d5cf95c
swmac <shaynefw@gmail.com>
2023-03-05 20:54:22 -0500

    fixed username import

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -14,6 +14,9 @@ router.get('/', async (req, res) => {
       posts,
       logged_in: req.session.logged_in
     });
+
+    console.log(posts);
+
   } catch (err) {
     res.status(500).json(err);
   }
