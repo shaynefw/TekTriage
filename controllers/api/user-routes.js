b63796b56e05bdab8d082b9f0c517aef201fa3e7
swmac <shaynefw@gmail.com>
2023-03-06 08:00:28 -0500

    added post page and fixed comments section

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,6 +13,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+// get user by id
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,10 +31,12 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render('single-post', {
+    res.render('post', {
       post,
       logged_in: req.session.logged_in
     });
+
+    console.log(post);
   } catch (err) {
     res.status(500).json(err);
   }
