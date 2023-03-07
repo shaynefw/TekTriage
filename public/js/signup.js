bbe101ceb716ca9e413596d5eb5a108a4b2ea7c5
swmac <shaynefw@gmail.com>
2023-03-06 19:14:18 -0500

    corrected sign in and signup, login and logout pages and functionality

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) { // if username and password are not empty
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+  
